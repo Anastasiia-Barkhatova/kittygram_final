9af13581ace7e711e998d7c76b83b57ff93fb3b7
Anastasiia Barkhatova <nastyabarhatova@mail.ru>
2024-08-08 19:08:01 +0300

    fix env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,8 +1,11 @@
 # flake8: noqa
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
