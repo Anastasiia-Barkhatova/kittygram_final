9cd5bcaae12a7cbc1a0ab2273ee4950971617b4b
Anastasiia Barkhatova <nastyabarhatova@mail.ru>
2024-08-08 21:12:42 +0300

    kkkk

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # DEBUG = bool(os.getenv('DEBUG', 'True').lower())
 DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
 
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 # ALLOWED_HOSTS = ['158.160.76.66', '127.0.0.1', 'localhost', 'mykittygram.zapto.org']
 
