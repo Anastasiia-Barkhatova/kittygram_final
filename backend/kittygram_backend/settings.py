700c0de8eff22bab69a22029f820a475c4cd6d94
Anastasiia Barkhatova <nastyabarhatova@mail.ru>
2024-08-09 07:04:31 +0300

    change allowed hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,11 @@
 # flake8: noqa
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
 # from django.core.management.utils import get_random_secret_key
 
-# load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,7 +13,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'zvbfdgyutk')
 
 DEBUG = True 
 
-ALLOWED_HOSTS = ['158.160.76.66', '127.0.0.1', 'localhost', 'mykittygram.zapto.org'] 
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
+
+# ALLOWED_HOSTS = ['158.160.76.66', '127.0.0.1', 'localhost', 'mykittygram.zapto.org'] 
 
 # SECRET_KEY = os.getenv('SECRET_KEY', 'zvbfdgyutk')
 # # SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
